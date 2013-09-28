7c20c334780c76c839d748de16f99abd920b5745
roman <roman@roman-ThinkPad-T430s.(none)>
2013-09-28 18:04:25 +0200

    firstcommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CyberCoachClient::Application.config.secret_key_base = '4b1d1b44abee79d94a48feb7fdb8580f4ba81a8ee2c5c3346b0b478e990c18d6e5e085992da1bc8e83c9626e2cdac696b01d48670f515e390aae71cabb8e7992'
